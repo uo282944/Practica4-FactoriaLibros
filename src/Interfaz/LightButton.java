f0ffd2c1c0f99a14a5e3fa87d0af3fb5843b0728
Javier <uo282944@uniovi.es>
2023-11-02 20:31:30 +0100

    Modificación de colores en la interfaz clara

--- src/Interfaz/LightButton.java
+++ src/Interfaz/LightButton.java
@@ -4,7 +4,8 @@ import java.awt.Color;
 
 public class LightButton extends Button{
     public LightButton() {
-        this.colorFondoBoton = new Color(104, 18, 6);
-        this.colorLetras = new Color(219, 238, 160);
+
+        this.colorFondoBoton = new Color(213, 212, 211);
+        this.colorLetras = new Color(38, 12, 83);
     }
 }
